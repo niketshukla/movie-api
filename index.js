d340b99a76d5737ca5ec2665aae7f1c95c0be5e0
Niket Shukla <niketshukla@gmail.com>
2022-01-21 16:26:03 -0500

    If applied will change index.js

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require("express"),
+  morgan = require("morgan");
 
 const app = express();
 
-let myLogger = (req, res, next) => {
-  console.log(req.url);
-  next();
-};
+let topMovies = [
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    director: "J.K. Rowling",
+  },
+  {
+    title: "Lord of the Rings",
+    director: "J.R.R. Tolkien",
+  },
+  {
+    title: "Twilight",
+    director: "Stephanie Meyer",
+  },
+  {
+    title: "John Wick",
+    director: "Chad Stahelski",
+  },
+  {
+    title: "American Sniper",
+    director: "Clint Eastwood",
+  },
+  {
+    title: "Rocky",
+    director: "John G. Avildsen",
+  },
+  {
+    title: "300",
+    director: "Zack Snyder",
+  },
+];
 
 app.use(morgan("common"));
 
+// app.get("/", (req, res) => {
+//   res.send("Welcome to my app!");
+// });
 app.get("/", (req, res) => {
-  res.send("Welcome to my app!");
+  res.send("Welcome to Myflix");
 });
 
-app.get("/secreturl", (req, res) => {
-  res.send("This is a secret url with super top-secret content.");
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: __dirname });
+});
+
+app.get("/top-movies", (req, res) => {
+  res.json(topMovies);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(8080, () => {
