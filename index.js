2514795d4ea7ba6626645e470c46acc021c60880
Niket Shukla <niketshukla@gmail.com>
2022-01-27 10:30:48 -0500

    Implemented Create, Read, Update methods on movies and users object

--- index.js
+++ index.js
@@ -9,38 +9,87 @@ app.use(bodyParser.json());
 let movies = [
   {
     title: "Harry Potter and the Sorcerer's Stone",
-    genre: "Fantasy Fiction",
-    director: "J.K. Rowling",
+    genre: {
+      name: "Fantasy Fiction",
+      description: "Lorem Ipsum",
+    },
+    director: {
+      name: "J.K. Rowling",
+      bio: "Lorem Ipsum",
+      dob: 1990,
+    },
   },
   {
     title: "Lord of the Rings",
-    genre: "Adventure Fiction",
-    director: "J.R.R. Tolkien",
+    genre: {
+      name: "Adventure Fiction",
+      description: "Lorem Ipsum",
+    },
+    director: {
+      name: "J.R.R. Tolkien",
+      bio: "Lorem Ipsum",
+      dob: 1990,
+    },
   },
   {
     title: "Twilight",
-    genre: "Romance Horror",
-    director: "Stephanie Meyer",
+    genre: {
+      name: "Romance Horror",
+      description: "Lorem Ipsum",
+    },
+    director: {
+      name: "Stephanie Meyer",
+      bio: "Lorem Ipsum",
+      dob: 1990,
+    },
   },
   {
     title: "John Wick",
-    genre: "Action",
-    director: "Chad Stahelski",
+    genre: {
+      name: "Action",
+      description: "Lorem Ipsum",
+    },
+    director: {
+      name: "Chad Stahelski",
+      bio: "Lorem Ipsum",
+      dob: 1990,
+    },
   },
   {
     title: "American Sniper",
-    genre: "Action",
-    director: "Clint Eastwood",
+    genre: {
+      name: "Action",
+      description: "Lorem Ipsum",
+    },
+    director: {
+      name: "Clint Eastwood",
+      bio: "Lorem Ipsum",
+      dob: 1990,
+    },
   },
   {
     title: "Rocky",
-    genre: "Action",
-    director: "John G. Avildsen",
+    genre: {
+      name: "Action",
+      description: "Lorem Ipsum",
+    },
+    director: {
+      name: "John G. Avildsen",
+      bio: "Lorem Ipsum",
+      dob: 1990,
+    },
   },
   {
     title: "300",
-    genre: "Action",
-    director: "Zack Snyder",
+    genre: {
+      name: "Action",
+      description: "Lorem Ipsum",
+    },
+    director: {
+      name: "Zack Snyder",
+      bio: "Lorem Ipsum",
+      dob: 1990,
+    },
   },
 ];
 
@@ -64,19 +113,19 @@ let users = [
 
 // use morgan for loggin into the log.txt
 app.use(morgan("common"));
-
+// Read
 app.get("/", (req, res) => {
   res.send("Welcome to Myflix");
 });
-
+// Read
 app.get("/documentation", (req, res) => {
   res.sendFile("public/documentation.html", { root: __dirname });
 });
-
+// Read
 app.get("/movies", (req, res) => {
   res.json(movies);
 });
-
+// Read
 app.get("/movies/:title", (req, res) => {
   res.json(
     movies.find((movie) => {
@@ -84,31 +133,31 @@ app.get("/movies/:title", (req, res) => {
     })
   );
 });
-
-app.get("/movies/:genre", (req, res) => {
-  res.json(
-    movies.find((movie) => {
-      return movie.genre === req.params.genre;
-    })
-  );
+// Read
+app.get("/movies/genre/:genreName", (req, res) => {
+  res.json(movies.find((movie) => movie.genre.name === req.params.genreName).genre);
 });
-
-app.get("/movies/:director", (req, res) => {
-  res.json(
-    movies.find((movie) => {
-      return movie.director === req.params.director;
-    })
-  );
+// Read
+app.get("/movies/director/:directorName", (req, res) => {
+  res.json(movies.find((movie) => movie.director.name === req.params.directorName).director);
 });
-
+// Create
 app.post("/users", (req, res) => {
-  res.send("New user registered");
-});
+  let newUser = req.body;
 
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("Please give name to the user");
+  }
+});
+// Update
 app.put("/users/:name", (req, res) => {
   res.send("User name modified");
 });
-
+// Create
 app.post("/users/:favorites", (req, res) => {
   res.send("Movie added to favorites");
 });
