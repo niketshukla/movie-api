95176843ce8551bf72be883f172964e580fe1258
Niket Shukla <niketshukla@gmail.com>
2022-05-13 13:13:33 -0400

    added single user endpoint

--- index.js
+++ index.js
@@ -101,6 +101,17 @@ app.get("/users", passport.authenticate("jwt", { session: false }), (req, res) =
       res.status(500).send("Error: " + err);
     });
 });
+// Get a user by username
+app.get('/users/:username', passport.authenticate("jwt", { session: false }), (req, res) => {
+  Users.findOne({ username: req.params.username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 // Create new user
 app.post(
   "/users",
